119cd9e46be518882b2e933c1444beb79733ec37
ktopchiev <karlostopchiev@gmail.com>
2024-06-30 17:42:48 +0300

    added handleCheck and handleDelete events

--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,7 @@ const Content = () => {
     const [items, setItems] = useState([
         {
             id: 1,
-            checked: false,
+            checked: true,
             item: "One half pound bag of Cocoa Covered Almonds Unsalted"
         },
         {
@@ -21,23 +21,44 @@ const Content = () => {
         }
     ]);
 
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems);
+        localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    }
+
     return (
         <main>
-            <ul>
-                {items.map((item) => (
-                    <li className="item" key={item.id}>
-                        <input
-                            type="checkbox"
-                            checked={item.checked}
-                        />
-                        <label>{item.item}</label>
-                        <FaTrashAlt 
-                            role="button" 
-                            tabIndex="0"
-                        />
-                    </li>
-                ))}
-            </ul>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label 
+                                style={(item.checked) ? {textDecoration: 'line-through'} : null}
+                                onDoubleClick={() => handleCheck(item.id)}
+                            >{item.item}</label>
+                            <FaTrashAlt 
+                                onClick={() => handleDelete(item.id)}
+                                role="button" 
+                                tabIndex="0"
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}> Your list is empty.</p>
+            )}
         </main>
     )
 }
