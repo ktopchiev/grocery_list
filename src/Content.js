2dcccb9a5d81a24def5b776fc123506050914a2d
ktopchiev <karlostopchiev@gmail.com>
2024-06-30 15:10:48 +0300

    add events to content component

--- src/Content.js
+++ src/Content.js
@@ -4,12 +4,28 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
+
+    const handleClick = () => {
+        console.log('You clicked it')
+    }
+    
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
     return (
         <main>
             <p>
                 {/* We can use expression like arrow function to show some results */}
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2('Dave')}>Click it</button>
+            <button onClick={(e) => handleClick3(e)}>Click it</button>
         </main>
     )
 }
