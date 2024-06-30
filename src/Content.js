5eb3066dcaf91d537cc71121fe5d01ba1bfe7981
ktopchiev <karlostopchiev@gmail.com>
2024-06-30 17:01:43 +0300

    clean up code and add items to the useState

--- src/Content.js
+++ src/Content.js
@@ -2,35 +2,27 @@ import { useState } from "react";
 
 const Content = () => {
 
-    const [name, setName] = useState('Karol');
-
-    const handleNameChange = () => {
-        const names = ['Karol', 'Petar', 'Michael'];
-        const int = Math.floor(Math.random() * 3);
-        setName(names[int]);
-      }
-
-    const handleClick = () => {
-        console.log('You clicked it')
-    }
-    
-    const handleClick2 = (n) => {
-        console.log(`${n} was clicked`)
-    }
-    
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
 
     return (
         <main>
-            <p>
-                {/* We can use expression like arrow function to show some results */}
-                Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change name</button>
-            <button onClick={() => handleClick2('Dave')}>Click it</button>
-            <button onClick={(e) => handleClick3(e)}>Click it</button>
+            
         </main>
     )
 }
