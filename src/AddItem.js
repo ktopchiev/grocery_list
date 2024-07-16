02c86cf0d7fb546cc295a308d054cea6f6fd73be
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-16 11:25:14 +0300

    set the form elements

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import {FaPlus} from 'react-icons/fa';
 
 const AddItem = () => {
   return (
     <form className='addForm'>
-        <label>Add new item</label>
-        <input />
-        <button></button>
+        <label htmlFor='addItem'>Add new item</label>
+        <input
+            autoFocus
+            id='addItem'
+            type='text'
+            placeholder='Add new item'
+            required        
+        />
+        <button
+            type='submit'
+            aria-label='Add item'
+        >
+            <FaPlus />
+        </button>
     </form>
   )
 }
