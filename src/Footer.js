448598e34edf442acee12add38ef7e348456d0b8
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-16 09:00:06 +0300

    .

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = ({ length }) => {
 
     return (
         <footer>
-            <p> { length } List {length === 1 ? "item" : "items" } </p>
+            <p> { length } list {length === 1 ? "item" : "items" } </p>
         </footer>
     )
 }
--- src/LineItem.js
+++ src/LineItem.js
@@ -12,7 +12,9 @@ const LineItem = ({ item, handleCheck, handleDelete }) => {
             <label 
                 style={(item.checked) ? {textDecoration: 'line-through'} : null}
                 onDoubleClick={() => handleCheck(item.id)}
-            >{item.item}</label>
+            >
+                {item.item}
+            </label>
             <FaTrashAlt 
                 onClick={() => handleDelete(item.id)}
                 role="button" 
