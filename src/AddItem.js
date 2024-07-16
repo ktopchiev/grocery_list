c52bbba26a16054d9fea78a372c783349e4ea4e9
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-16 11:11:38 +0300

    added new additem file and css for the add item form

---
+++ src/AddItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddItem = () => {
+  return (
+    <form className='addForm'>
+        <label>Add new item</label>
+        <input />
+        <button></button>
+    </form>
+  )
+}
+
+export default AddItem
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from "react";
@@ -38,6 +39,7 @@ const handleDelete = (id) => {
   return (
     <div className="App" >
       <Header title="Grocery List" />
+      <AddItem />
       <Content
         items={items} 
         handleCheck={handleCheck} 
