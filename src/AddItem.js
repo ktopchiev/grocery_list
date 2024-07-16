a912582fdfcd18839825c067931413cb9a4534ab
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-16 15:43:04 +0300

    added addItem functionality using local storage

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import {FaPlus} from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 
-const AddItem = () => {
-  return (
-    <form className='addForm'>
-        <label htmlFor='addItem'>Add new item</label>
-        <input
-            autoFocus
-            id='addItem'
-            type='text'
-            placeholder='Add new item'
-            required        
-        />
-        <button
-            type='submit'
-            aria-label='Add item'
-        >
-            <FaPlus />
-        </button>
-    </form>
-  )
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    return (
+        <form className='addForm' onSubmit={handleSubmit}>
+            <label htmlFor='addItem'>
+                Add item
+            </label>
+            <input
+                id='addItem'
+                type='text'
+                placeholder='Add Item'
+                required
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+            />
+            <button
+                type='submit'
+                aria-label='Add Item'
+            >
+                <FaPlus />
+            </button>
+        </form>
+    )
 }
 
 export default AddItem
