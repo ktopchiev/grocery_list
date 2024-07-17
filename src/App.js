07c9f74e583d52d28071753019302e37c4683ed1
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-17 16:21:06 +0300

    Added comments to app functions

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
+  //If you’re not trying to synchronize with some external system, you probably don’t need an Effect.
+  //To connect your component to some external system, call useEffect at the top level of your component.
   useEffect(() => {
     localStorage.setItem('shoppingList', JSON.stringify(items));
   }, [items])
 
+  //Adding new item to list of items
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
@@ -22,16 +25,19 @@ function App() {
     setItems(listItems);
   }
 
+  //If some of the items is checked, this will change the checked property of the object, according to users action
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
   }
 
+  //Delete an item from the items list
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
   }
 
+  //Submit the additions through the form
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
